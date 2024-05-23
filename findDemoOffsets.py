f4a41bd04516983c314376fd95cb2f9ae97d38fe
drsparklegasm <Jromega3@gmail.com>
2024-05-23 13:50:34 -0700

    Fixed exports

--- findDemoOffsets.py
+++ findDemoOffsets.py
@@ -7,11 +7,8 @@ demoFile = open(filename, 'rb')
 demoData = demoFile.read()
 
 offsets = []
-
-
+os.makedirs('extractedDemos', exist_ok=True)
 opening = b'\x10\x08\x00\x00\x05\x00\x00\x00'
-print(opening)
-print(demoData[0:8])
 
 def findDemoOffsets():
     offset = 0
@@ -30,15 +27,18 @@ def findDemoOffsets():
         print(offset.to_bytes(4, 'big').hex())
 
 def splitDemoFiles():
-    for i in range(len(offsets)):
-        if i == len(offsets):
-            f = open(f'demo-{i}.txt', 'wb')
-            f.write(demoData[offsets[i]:len(demoData)])
-            f.close()
-        else:   
-            f = open(f'demo-{i}.txt', 'wb')
-            f.write(demoData[offsets[i]:offsets[i+1]])
-            f.close()
+    for i in range(len(offsets)):  
+        start = offsets[i] 
+        if i < len(offsets) - 1:
+            end = offsets[i + 1]
+        else:
+            end = len(demoData) - 1
+        f = open(f'extractedDemos/demo-{i + 1}.bin', 'wb')
+        f.write(demoData[start:end])
+        f.close()
+        print(f'Demo {i} written!')
+    
+    print(f'{i+1} demo files written!')
 
 
 if __name__ == '__main__':
