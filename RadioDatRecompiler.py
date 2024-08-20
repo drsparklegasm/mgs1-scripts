973202136616be4a2e7610714820ccd18062f116
Jromega3@gmail.com <drsparklegasm>
2024-08-19 17:09:13 -0700

    Fix error

--- RadioDatRecompiler.py
+++ RadioDatRecompiler.py
@@ -11,15 +11,7 @@ We will output to RADIO.DAT or optionally a filename of your choice.
 
 """
 
-
-def realignOffsets():
-    print(f'Offset integrity reviewed and done')
-
-def createBinary():
-    print(f'Binary data created')
-
-def outputFile():
-    print(f'File has been saved!')
+# ==== Dependencies ==== #
 
 import os, struct
 import radioDict 
@@ -31,25 +23,50 @@ inputXML = 'usaD1Analyze.xml'
 
 radioSource = ET.parse(inputXML)
 
+# ==== DEFS ==== #
+
+# Large steps here
+def realignOffsets():
+    print(f'Offset integrity reviewed and done')
+
+def createBinary():
+    print(f'Binary data created')
+
+def outputFile():
+    print(f'File has been saved!')
+
+# ==== Byte Encoding Defs ==== #
+
 def getCallHeaderBytes(call: ET.Element) -> bytes:
     """
     Returns the bytes used for a call header. Should always return bytes object with length 11.
     """
     callHeader = b''
-    callAtts = call.attrib
-    frequency = int(float(callAtts.get("Freq")) * 100)
+    attrs = call.attrib
+    frequency = int(float(attrs.get("Freq")) * 100)
     freqBytes = struct.pack('>H', frequency)
 
-    unk1 = bytes.fromhex(callAtts.get("UnknownVal1"))
-    unk2 = bytes.fromhex(callAtts.get("UnknownVal2"))
-    unk3 = bytes.fromhex(callAtts.get("UnknownVal3"))
-    lengthBytes = struct.pack('>H', int(callAtts.get("Length")) - 9) # The header is 11 bytes, not in the length as shown in bytes
+    unk1 = bytes.fromhex(attrs.get("UnknownVal1"))
+    unk2 = bytes.fromhex(attrs.get("UnknownVal2"))
+    unk3 = bytes.fromhex(attrs.get("UnknownVal3"))
+    lengthBytes = struct.pack('>H', int(attrs.get("Length")) - 9) # The header is 11 bytes, not in the length as shown in bytes
 
     # Assemble all pieces
     callHeader = freqBytes + unk1 + unk2 + unk3 + bytes.fromhex('80') + lengthBytes
     return callHeader
 
-# Recompile call headers:
+def getSubtitleBytes(subtitle: ET.Element) -> bytes:
+    """
+    Returns the hex for an entire subtitle command. Starts with FF01 and always ends with one null byte (\x00)
+    """
+    attrs = subtitle.attrib
+    subtitleBytes = bytes.fromhex('ff01')
+    
+
+
+
+
+# Test code: Recompile call headers
 for call in radioSource.findall(".//Call"):
     bytesToPrint = getCallHeaderBytes(call)
     print(bytesToPrint.hex())
