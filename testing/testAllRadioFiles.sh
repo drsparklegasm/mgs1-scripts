72081978ba2441b7b8bc3cea0489530098db514c
drsparklegasm <Jromega3@gmail.com>
2024-09-06 15:47:16 -0700

    testing new graphics matching

--- testing/testAllRadioFiles.sh
+++ testing/testAllRadioFiles.sh
@@ -13,22 +13,20 @@ different_count=0
 for input in "$input_dir"/*.DAT; do
     base_filename=$(basename "$input" .DAT)
     echo $base_filename
-    python3 $SPLITSCRIPT $input "$output_dir/$base_filename" -xz
+    python3 $SPLITSCRIPT $input "$output_dir/$base_filename" -xzg
 done
 
-for original in "$output_dir"/*.xml; do
-    base_filename=$(basename "$original" .xml)
-    python3 $RECOMPILESCRIPT "$output_dir/$base_filename.xml" "$output_dir/$base_filename-mod.DAT" -x   
-    if diff "$input_dir/$base_filename.DAT" "$output_dir/$base_filename-mod.DAT" >/dev/null; then
-        echo "Files are the same: $original"
-        ((same_count++))
-    else
-        echo "Files are different: $original"
-        ((different_count++))
-    fi
-done
-
-echo "Total files that are the same: $same_count"
-echo "Total files that are different: $different_count"
-
-rm recompiledCallBins/*.log
+# for original in "$output_dir"/*.xml; do
+#     base_filename=$(basename "$original" .xml)
+#     python3 $RECOMPILESCRIPT "$output_dir/$base_filename.xml" "$output_dir/$base_filename-mod.DAT" -x   
+#     if diff "$input_dir/$base_filename.DAT" "$output_dir/$base_filename-mod.DAT" >/dev/null; then
+#         echo "Files are the same: $original"
+#         ((same_count++))
+#     else
+#         echo "Files are different: $original"
+#         ((different_count++))
+#     fi
+# done# 
+# echo "Total files that are the same: $same_count"
+# echo "Total files that are different: $different_count"# 
+# rm recompiledCallBins/*.log
