eb4355f2ad9291f1ec030e0b8de9b5f2a091fe8c
drsparklegasm <Jromega3@gmail.com>
2024-09-06 23:02:24 -0700

    Add variable to stageDirAnalyzer

--- StageDirTools/analyzeStageDirFiles.sh
+++ StageDirTools/analyzeStageDirFiles.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-for script in $(find . -name '*.gcx'); do
+stage_dir="radioDatFiles/stage-jpn/"
+
+for script in $(find $stage_dir -name '*.gcx'); do
     echo $script
     python3 myScripts/StageDirTools/callsInStageDirFinder.py $script
 done
