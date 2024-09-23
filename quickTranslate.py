370b25dc9552faaa925c712b76d70858515514cc
drsparklegasm <Jromega3@gmail.com>
2024-09-22 21:24:31 -0700

    template-ize quickTranslate

--- quickTranslate.py
+++ quickTranslate.py
@@ -1,7 +1,5 @@
-from radioTools import radioDict as RD
+import radioTools.radioDict as RD
 
-text = "9C 15 9C 16 81 2E 82 17 C2 43 D0 06 82 3E 82 53 20 82 30 82 4B 82 0B 82 53 C0 7F 82 4C 82 04 82 36 82 53 00"
+text = ""
 textToPrint = RD.translateJapaneseHex(bytes.fromhex(text), callDict = {} )
 print(textToPrint)
-
-# FOX HOUND 部隊と彼等の率いる次世代特殊部隊が
