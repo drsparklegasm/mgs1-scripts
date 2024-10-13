26a63a38d15956154e90b299fd3c8964dc4079ec
Jromega3@gmail.com <drsparklegasm>
2024-10-13 13:18:19 -0700

    Decoding logic done

--- creditsHacking/creditsHacking.py
+++ creditsHacking/creditsHacking.py
@@ -17,46 +17,88 @@ Notes from Green Goblin:
 
 """
 
-creditsFilename = ""
+creditsFilename = "creditsHacking/00b8ba.rar"
 creditsData = open(creditsFilename, 'rb').read()
 
-
+class imageData:
+    
+    def __init__(self, allData: bytes):
+        self.width = struct.unpack('<H', allData[0:2])[0]
+        self.height = struct.unpack('<H', allData[2:4])[0]
+        self.palette = allData[4:36]
+        self.dataLength = struct.unpack('<L', allData[36:40])[0]
+        self.compData = allData[40: 40 + self.dataLength]
+        
 
 def decompressBytes(gfxData: bytes, size: tuple [int, int]) -> bytes:
     newGfxBytes = b''
     lines = []
     width, height = size
     length = len(gfxData)
+        
 
     pointer = 0
     while pointer < length:
         command = gfxData[pointer]
+        print(f'Command bytes: {gfxData[pointer:pointer+2]}')
         match command:
             case 0x00:
-                print(f'Zero. End of a line!')
                 lines.append(newGfxBytes)
+                if len(newGfxBytes) != int(width / 2):
+                    print(f'Line did not match! Width: {width}, bytes: {len(newGfxBytes)}. Should be doubled!')
+                newGfxBytes = b''
                 pointer += 1
             case 0x40:
-                newGfxBytes += b'' * width
+                newGfxBytes += bytes.fromhex('ff') * int(width / 2)
+                lines.append(newGfxBytes)
+                newGfxBytes = b''
                 pointer += 1
             case 0x80:
-                newGfxBytes += b'' * width
+                newGfxBytes += bytes.fromhex('00') * int(width / 2)
+                lines.append(newGfxBytes)
+                newGfxBytes = b''
                 pointer += 1
             case _:
-                print(f'Case not 0x0, 0x40, 0x80')
                 if command < 0x80:
-                    print(f'adding {command} bytes in front...')
                     dataToAdd = gfxData[pointer + 1: pointer + 1 + command]
                     newGfxBytes += dataToAdd
                     pointer += command + 1
                 elif command > 0x80:
                     numBytesToAdd = command - 0x80
                     position = 0 - gfxData[pointer + 1]
-                    print(f'Repeating {command} bytes behind...')
-                    bytesToRepeat = newGfxBytes[position:position + numBytesToAdd]
-                    print(bytesToRepeat.hex())
-                    newGfxBytes += bytesToRepeat
+                    if numBytesToAdd > (0 - position):
+                        bytesToRepeat = newGfxBytes[position:]
+                    else:
+                        bytesToRepeat = newGfxBytes[position:position + numBytesToAdd]
+                    newGfxBytes += bytesToRepeat * int(numBytesToAdd / len(bytesToRepeat))
                     pointer += 2
 
+    print(f'{width} x {height}')
+    for line in lines:
+        print(f'{len(line)}')
+    print(f'Height is {height}, we have {len(lines)} lines!')
     return newGfxBytes
 
+def getImages(fileData: bytes) -> list:
+    images = []
+    numImg = struct.unpack('<L', fileData[0:4])[0]
+    i = 0
+    nextImage = 4
+    while i < numImg:
+        images.append(imageData(fileData[nextImage:]))
+        nextImage += images[-1].dataLength + 40
+        i += 1
+    
+    return images
+
+# def analyzeImage(imgData: bytes) -> tuple[bytes, int, int, bytes]:
+
+if __name__ == "__main__":
+    print(f'Outputting graphics from {creditsFilename}')
+    imageList = getImages(creditsData)
+    print(f'{len(imageList)} images found!')
+    for image in imageList:
+        size = (image.width, image.height)
+        decompressBytes(image.compData, size)
+    
+    
