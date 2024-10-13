25ae4b7d1a0b7ff51b0b35898006096363c4d312
drsparklegasm <Jromega3@gmail.com>
2024-10-12 21:34:13 -0700

    update logic

--- creditsHacking/creditsHacking.py
+++ creditsHacking/creditsHacking.py
@@ -15,8 +15,6 @@ Notes from Green Goblin:
 0x00 == End of a line
 0x40 == a full line with the last color of the palette
 
-
-
 """
 
 creditsFilename = ""
@@ -24,12 +22,41 @@ creditsData = open(creditsFilename, 'rb').read()
 
 
 
-def extractGraphic():
+def decompressBytes(gfxData: bytes, size: tuple [int, int]) -> bytes:
+    newGfxBytes = b''
+    lines = []
+    width, height = size
+    length = len(gfxData)
 
-    return
+    pointer = 0
+    while pointer < length:
+        command = gfxData[pointer]
+        match command:
+            case 0x00:
+                print(f'Zero. End of a line!')
+                lines.append(newGfxBytes)
+                pointer += 1
+            case 0x40:
+                newGfxBytes += b'' * width
+                pointer += 1
+            case 0x80:
+                newGfxBytes += b'' * width
+                pointer += 1
+            case _:
+                print(f'Case not 0x0, 0x40, 0x80')
+                if command < 0x80:
+                    print(f'adding {command} bytes in front...')
+                    dataToAdd = gfxData[pointer + 1: pointer + 1 + command]
+                    newGfxBytes += dataToAdd
+                    pointer += command + 1
+                elif command > 0x80:
+                    numBytesToAdd = command - 0x80
+                    position = 0 - gfxData[pointer + 1]
+                    print(f'Repeating {command} bytes behind...')
+                    bytesToRepeat = newGfxBytes[position:position + numBytesToAdd]
+                    print(bytesToRepeat.hex())
+                    newGfxBytes += bytesToRepeat
+                    pointer += 2
 
-def processLine():
-    """
-    A line ends on \x00
-    """
-    return
+    return newGfxBytes
+
