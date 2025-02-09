f9d3a42f17fb001a7fb76bc73554241f9dd003d1
drsparklegasm <Jromega3@gmail.com>
2025-02-09 15:16:29 -0800

    test stuff

--- testing/runJpnBuildTest.sh
+++ testing/runJpnBuildTest.sh
@@ -7,7 +7,9 @@
 
 # use Programatic replacement
 
-
+# # Extracting and automating translation (disk 1)
+# /bin/python3 /home/solidmixer/projects/mgs1-undub/myScripts/RadioDatTools.py -jzx build-src/jpn-d1/MGS/RADIO.DAT radioWorkingDir/jpn-d1/RADIO 
+# /bin/python3 /home/solidmixer/projects/mgs1-undub/radioWorkingDir/dialogueSwap.py
 
 # This area re-compiles a RADIO file for jpn
 python3 myScripts/xmlModifierTools.py inject radioWorkingDir/output.json radioWorkingDir/jpn-d1/RADIO.xml 
