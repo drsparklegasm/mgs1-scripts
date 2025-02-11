906e06a0988d9b7e95b3a0ca014d9cb959ca1dac
drsparklegasm <Jromega3@gmail.com>
2025-02-10 22:50:36 -0800

    Char updates

--- testing/runJpnBuildTest.sh
+++ testing/runJpnBuildTest.sh
@@ -11,9 +11,15 @@
 # /bin/python3 /home/solidmixer/projects/mgs1-undub/myScripts/RadioDatTools.py -jzx build-src/jpn-d1/MGS/RADIO.DAT radioWorkingDir/jpn-d1/RADIO 
 # /bin/python3 /home/solidmixer/projects/mgs1-undub/radioWorkingDir/dialogueSwap.py
 
+# Inject graphics data (STAGE.DIR) for disk 1 ONLY for now
+wine goblin-tools/ninja.exe -i /home/solidmixer/projects/mgs1-undub/stageGraphicsWorking/jpn-d1/ -pack -o stageGraphicsWorking/out/STAGE-j1.DIR -img
+# wine goblin-tools/ninja.exe -i /home/solidmixer/projects/mgs1-undub/stageGraphicsWorking/jpn-d2/ -pack -o stageGraphicsWorking/out/STAGE-j2.DIR -img 
+
+
 # This area re-compiles a RADIO file for jpn
 python3 myScripts/xmlModifierTools.py inject radioWorkingDir/output.json radioWorkingDir/jpn-d1/RADIO.xml 
-python3 myScripts/RadioDatRecompiler.py -p radioWorkingDir/jpn-d1/RADIO-merged.xml radioWorkingDir/jpn-d1/new-RADIO.DAT -s build-src/jpn-d1/MGS/STAGE.DIR -S radioWorkingDir/jpn-d1/new-STAGE.DIR
+# python3 myScripts/RadioDatRecompiler.py -p radioWorkingDir/jpn-d1/RADIO-merged.xml radioWorkingDir/jpn-d1/new-RADIO.DAT -s build-src/jpn-d1/MGS/STAGE.DIR -S radioWorkingDir/jpn-d1/new-STAGE.DIR
+python3 myScripts/RadioDatRecompiler.py -p radioWorkingDir/jpn-d1/RADIO-merged.xml radioWorkingDir/jpn-d1/new-RADIO.DAT -s stageGraphicsWorking/out/STAGE-j1.DIR -S radioWorkingDir/jpn-d1/new-STAGE.DIR
 
 # This area compiles the new DEMO.DAT and adds it to the disk image (D1)
 python3 myScripts/DemoTools/demoTextInjector.py # currently hard coded
