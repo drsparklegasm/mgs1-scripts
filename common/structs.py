f3182cfe1fbaf19b91076cb9bd6d5a19faa0d302
drsparklegasm <Jromega3@gmail.com>
2025-02-14 11:30:48 -0800

    Make new common structs

---
+++ common/structs.py
@@ -0,0 +1,47 @@
+"""
+Commonly used structures for MGS dialogue lines will go here.
+
+demoSub is one of the big ones as the similar thing is used in demo, vox, zmovie.
+"""
+import sys, struct, os
+sys.path.append(os.path.abspath('./myScripts'))
+
+import radioTools.radioDict as RD
+
+
+class subtitle:
+    text: str
+    startFrame: int
+    duration: int
+
+    def __init__(self, dialogue, b, c) -> None:
+        self.text = dialogue
+        self.startFrame = int(b)
+        self.duration = int(c)
+
+        return
+    
+    def __init__(self, rawBytes: bytes) -> None:
+        length, start, duration = struct.unpack("III", rawBytes[0:12])
+        self.text = rawBytes[16:].strip(bytes.fromhex("00"))
+        self.startFrame = int(start)
+        self.duration = int(duration)
+
+        return
+    
+    def __str__(self) -> str:
+        a = f'Subtitle contents: Start: {self.startFrame} Duration: {self.duration} Text: {self.text}'
+        return a
+    
+    def __bytes__(self) -> bytes:
+        """
+        Simple. Encodes the dialogue as bytes. 
+        Adds the buffer we need to be divisible by 4...
+        Return the new bytes.
+        """
+        subtitleBytes: bytes = struct.pack("III", self.startFrame, self.duration, 0)
+        subtitleBytes += RD.encodeJapaneseHex(self.text)[0]
+        bufferNeeded = 4 - (len(subtitleBytes) % 4)
+        subtitleBytes += bytes(bufferNeeded)
+        
+        return subtitleBytes
