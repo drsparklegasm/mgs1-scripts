1d84d9a05873de4a160f0a24686e010f274df19e
drsparklegasm <Jromega3@gmail.com>
2025-02-14 12:59:02 -0800

    fix abs pathing

--- DemoTools/demoTextExtractor.py
+++ DemoTools/demoTextExtractor.py
@@ -31,10 +31,10 @@ disc = 1
 
 # Create a directory to store the extracted texts
 # Get the files from the folder directory
-inputDir = f'/workingFiles/{version}-d{disc}/demo/bins'
-outputDir = f'/workingFiles/{version}-d{disc}/demo/texts'
+inputDir = f'workingFiles/{version}-d{disc}/demo/bins'
+outputDir = f'workingFiles/{version}-d{disc}/demo/texts'
 os.makedirs(outputDir, exist_ok=True)
-outputJsonFile = f"/workingFiles/{version}-d{disc}/demo/demoText-{version}.json"
+outputJsonFile = f"workingFiles/{version}-d{disc}/demo/demoText-{version}.json"
 
 # Grab all files in the directory and sort into order.
 bin_files = glob.glob(os.path.join(inputDir, '*.bin'))
