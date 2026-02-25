1ca7e1cdb9941aeba91b5dbc208ead367dbe27d1
DrSparklegasm <jromega3@gmail.com>
2026-02-24 23:22:17 -0800

    Adjusted files to take argparse inputs rather than hard coded -claude

--- DemoTools/demoRejoiner.py
+++ DemoTools/demoRejoiner.py
@@ -3,58 +3,64 @@ sys.path.append(os.path.abspath('./myScripts'))
 import re
 import glob
 import struct
+import argparse
 import progressbar
 import translation.radioDict as RD
 import json
 
 import DemoTools.demoTextExtractor as DTE
 
-version = "usa"
-version = "jpn"
-disc = 1
+parser = argparse.ArgumentParser(description='Rejoin .dmo bin files into DEMO.DAT')
+parser.add_argument('input', type=str, help='Input directory containing original .dmo bin files.')
+parser.add_argument('output', type=str, help='Output DEMO.DAT file path.')
+parser.add_argument('-n', '--new-bins', nargs='?', type=str, help='Directory containing new/modified .dmo files. Defaults to <input>/../newBins.')
+parser.add_argument('-d', '--offset-dump', nargs='?', type=str, help='Path to save offset JSON dump.')
 
 # Toggles
 debug = True
 
+def main(args=None):
+    if args is None:
+        args = parser.parse_args()
 
-# Directory configs
-inputDir = f'workingFiles/{version}-d{disc}/demo/bins'
-outputDir = f'workingFiles/{version}-d{disc}/demo/newBins'
-outputDemoFile = f'workingFiles/{version}-d{disc}/demo/new-DEMO.DAT'
-offsetDump = f'workingFiles/{version}-d{disc}/demo/newDemoOffsets.json'
-os.makedirs(outputDir, exist_ok=True)
+    inputDir = args.input
+    outputDemoFile = args.output
+    newBinsDir = args.new_bins if args.new_bins else os.path.join(inputDir, '../newBins')
+    offsetDump = args.offset_dump if args.offset_dump else os.path.join(os.path.dirname(outputDemoFile), 'newDemoOffsets.json')
 
-origBinFiles = glob.glob(os.path.join(inputDir, '*.dmo'))
-origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
+    os.makedirs(os.path.dirname(outputDemoFile) if os.path.dirname(outputDemoFile) else '.', exist_ok=True)
 
-newBinFiles = glob.glob(os.path.join(outputDir, '*.dmo'))
-origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
+    origBinFiles = glob.glob(os.path.join(inputDir, '*.dmo'))
+    origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
 
-newDemoBytes = b''
-newOffsets = {}
-with open(outputDemoFile, 'wb') as f:
-    for file in origBinFiles:
-        # Simple switch to use a new file if it exists
-        if file.replace('bins', 'newBins') in newBinFiles:
-            file = file.replace('bins', 'newBins') 
-            basename = file.split("/")[-1].split(".")[0]
-            print(f'{basename}: Using new version of the demo...')
-        else:
-            basename = file.split("/")[-1].split(".")[0]
-            print(f'{basename}: Using old file...\r', end="")
-        # In this section get the offsets to write to a json
-        demoNum = basename.split("-")[1]
-        demoStart = struct.pack(">L", len(newDemoBytes) // 0x800).hex()
-        newOffsets.update({demoNum: demoStart})
-        # Finally, add the demo to the master file
-        with open(file, 'rb') as demoBytes:
-            newDemoBytes += demoBytes.read()
+    newBinFiles = glob.glob(os.path.join(newBinsDir, '*.dmo'))
+    newBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
 
-    f.write(newDemoBytes)
-    f.close()
+    newDemoBytes = b''
+    newOffsets = {}
+    with open(outputDemoFile, 'wb') as f:
+        for file in origBinFiles:
+            new_file_path = os.path.join(newBinsDir, os.path.basename(file))
+            if new_file_path in newBinFiles:
+                file = new_file_path
+                basename = os.path.basename(file).split(".")[0]
+                print(f'{basename}: Using new version of the demo...')
+            else:
+                basename = os.path.basename(file).split(".")[0]
+                print(f'{basename}: Using old file...\r', end="")
+            demoNum = basename.split("-")[1]
+            demoStart = struct.pack(">L", len(newDemoBytes) // 0x800).hex()
+            newOffsets.update({demoNum: demoStart})
+            with open(file, 'rb') as demoBytes:
+                newDemoBytes += demoBytes.read()
 
-print(f'{outputDemoFile} was written!')
+        f.write(newDemoBytes)
 
-# Write offsets to file
-with open(offsetDump, 'w') as f:
-    json.dump(newOffsets, f)
+    print(f'{outputDemoFile} was written!')
+
+    with open(offsetDump, 'w') as f:
+        json.dump(newOffsets, f)
+
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
--- voxTools/voxRejoiner.py
+++ voxTools/voxRejoiner.py
@@ -3,58 +3,68 @@ sys.path.append(os.path.abspath('./myScripts'))
 import re
 import glob
 import struct
+import argparse
 import progressbar
 import translation.radioDict as RD
 import json
 
 import voxTools.voxTextExtractor as DTE
 
-version = "usa"
-version = "jpn"
-disc = 1
+parser = argparse.ArgumentParser(description='Rejoin .vox bin files into VOX.DAT')
+parser.add_argument('input', type=str, help='Input directory containing original .vox bin files.')
+parser.add_argument('output', type=str, help='Output VOX.DAT file path.')
+parser.add_argument('-s', '--source', nargs='?', type=str, help='Path to original VOX.DAT (used as fallback when new bins are exhausted).')
+parser.add_argument('-n', '--new-bins', nargs='?', type=str, help='Directory containing new/modified .vox files. Defaults to <input>/../newBins.')
 
 # Toggles
 debug = True
 
-# Directory configs
-originalVox = f'build-src/{version}-d{disc}/MGS/VOX.DAT'
-inputDir = f'workingFiles/{version}-d{disc}/vox/bins'
-outputDir = f'workingFiles/{version}-d{disc}/vox/newBins'
-outputvoxFile = f'workingFiles/{version}-d{disc}/vox/new-VOX.DAT'
-os.makedirs(outputDir, exist_ok=True)
+def main(args=None):
+    if args is None:
+        args = parser.parse_args()
 
-origBinFiles = glob.glob(os.path.join(inputDir, '*.bin'))
-origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
+    inputDir = args.input
+    outputvoxFile = args.output
+    originalVox = args.source
+    newBinsDir = args.new_bins if args.new_bins else os.path.join(inputDir, '../newBins')
 
-newBinFiles = glob.glob(os.path.join(outputDir, '*.bin'))
-origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
+    os.makedirs(os.path.dirname(outputvoxFile) if os.path.dirname(outputvoxFile) else '.', exist_ok=True)
 
-print(f'Building New VOX File...')
-newvoxBytes = b''
+    origBinFiles = glob.glob(os.path.join(inputDir, '*.vox'))
+    origBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
 
-count = 0
-with open(outputvoxFile, 'wb') as f:
-    for file in origBinFiles:
-        if count == len(newBinFiles):
-            print(f'\nAll new files injected. Using the remainder of original file...')
-            with open(originalVox, 'rb') as originalVox:
-                originalVox.seek(len(newvoxBytes))
-                newvoxBytes += originalVox.read()
+    newBinFiles = glob.glob(os.path.join(newBinsDir, '*.vox'))
+    newBinFiles.sort(key=lambda f: int(f.split('-')[-1].split('.')[0]))
+
+    print(f'Building New VOX File...')
+    newvoxBytes = b''
+
+    count = 0
+    with open(outputvoxFile, 'wb') as f:
+        for file in origBinFiles:
+            if count == len(newBinFiles):
+                if originalVox:
+                    print(f'\nAll new files injected. Using the remainder of original file...')
+                    with open(originalVox, 'rb') as origFile:
+                        origFile.seek(len(newvoxBytes))
+                        newvoxBytes += origFile.read()
                 break
-        elif file.replace('bins', 'newBins') in newBinFiles:
-            file = file.replace('bins', 'newBins') 
-            basename = file.split("/")[-1].split(".")[0]
-            print(f'{basename}: Using new {basename}...')
-            count += 1
-        else:
-            basename = file.split("/")[-1].split(".")[0]
-            print(f'{basename}: Using old version...\r', end="")
-        voxBytes = open(file, 'rb')
-        newvoxBytes += voxBytes.read()
-        voxBytes.close()
-    f.write(newvoxBytes)
-    f.close()
+            new_file_path = os.path.join(newBinsDir, os.path.basename(file))
+            if new_file_path in newBinFiles:
+                file = new_file_path
+                basename = os.path.basename(file).split(".")[0]
+                print(f'{basename}: Using new {basename}...')
+                count += 1
+            else:
+                basename = os.path.basename(file).split(".")[0]
+                print(f'{basename}: Using old version...\r', end="")
+            voxBytes = open(file, 'rb')
+            newvoxBytes += voxBytes.read()
+            voxBytes.close()
+        f.write(newvoxBytes)
 
-print(f'{outputvoxFile} was written!')
+    print(f'{outputvoxFile} was written!')
 
-            
+if __name__ == '__main__':
+    args = parser.parse_args()
+    main(args)
